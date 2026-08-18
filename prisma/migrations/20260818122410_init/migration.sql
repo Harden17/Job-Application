660d291d1b6f35ca233b0cdf2e16af5a408be622
Kayongo Emmanuel <kayongoemmanuelharden@gmail.com>
2026-08-18 15:26:09 +0300

    Add initial migration files for User and Job tables with constraints

---
+++ prisma/migrations/20260818122410_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Job" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "company" TEXT NOT NULL,
+    "jobTitle" TEXT NOT NULL,
+    "jobUrl" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Job" ADD CONSTRAINT "Job_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
