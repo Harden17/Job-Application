79921140e183c70aa5d40441ab3623e919738995
Kayongo Emmanuel <kayongoemmanuelharden@gmail.com>
2026-08-20 15:15:00 +0300

    fix: correct token extraction and user ID assignment in auth middleware

--- src/middleware/authmiddleware.js
+++ src/middleware/authmiddleware.js
@@ -4,10 +4,9 @@ dotenv.config();
 
 
 function authMiddleware(req, res, next) {
-    const authHeader = req.headers['authorization']; 
-    const token = authHeader && authHeader.split(' ')[1]; 
+    const token = req.headers['authorization']; 
     if (!token) {
-        return res.status(401).json({ message: 'Failed to authenticate user!'});
+        return res.status(401).json({ message: 'Failed to authenticate userA!'});
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
@@ -15,7 +14,7 @@ function authMiddleware(req, res, next) {
             return res.status(401).json({ message: 'Failed to authenticate user!' });
         }
         // this is the id we are gping to use to get the data of aspecific user.
-        req.userId = decoded.id;
+        req.userId = decoded.userId;
         next();
     });
 
