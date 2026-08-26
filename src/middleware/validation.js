a8231ee1f3add67d383247922a55a411e2f781f7
Kayongo Emmanuel <kayongoemmanuelharden@gmail.com>
2026-08-26 16:07:15 +0300

    refactor: restructure the validation middleware to accept multiple schemas

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,18 +1,25 @@
 import {z} from 'zod';
 
-// Define the zod schema for validation
-const userSchema = z.object({
+// Define Schemas 
+// User registration and login schema
+export const userSchema = z.object({
     email: z.string().email({ message: 'Invalid email address' }),
     password: z.string().min(6, { message: 'Password must be at least 6 characters long' }),
 });
+// 
+
 
 // create the validation middleware
-export const validateUser = (req, res, next) => {
-    const validationResult = userSchema.safeParse(req.body);
-    if (!validationResult.success) {
-        return res.status(400).json({ errors: validationResult.error.errors });
-    }
-    next();
-}
+export const validateUser = (schema) =>  {
+    return (req, res, next) => {
+        const validationResult = schema.safeParse(req.body);
+        if (!validationResult.success) {
+            return res.status(400).json({ errors: validationResult.error.errors });
+        }
+
+        req.body = validationResult.data; // Use the validated data
+        next();
+    };
+};  
 
 export default validateUser;
