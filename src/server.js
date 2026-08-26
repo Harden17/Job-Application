37af4738956921500ab258415f7f2cc0c4d73c86
Kayongo Emmanuel <kayongoemmanuelharden@gmail.com>
2026-08-26 16:10:03 +0300

    feat: update user validation middleware to use userSchema

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import path,{dirname} from "path";
 import { fileURLToPath } from "url";
 import approutes from "./routes/applicationroutes.js";
 import authMiddleware from "./middleware/authmiddleware.js";
-import validateUser from "./middleware/validation.js";
+import validateUser, {userSchema} from "./middleware/validation.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 // middleware
 app.use(express.json());
-app.use("/auth",validateUser, authroute);
+app.use("/auth",validateUser(userSchema), authroute);
 app.use("/jobs", authMiddleware, approutes);
 // Serve the frontend build files
 app.get("/", (req, res) => {
