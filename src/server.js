4e34f3d787626d0b77e56a25272b5174617cfaa1
Kayongo Emmanuel <kayongoemmanuelharden@gmail.com>
2026-09-03 15:52:20 +0300

    fix: correct path to index.html for serving frontend assets

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use("/jobs", authMiddleware, approutes);
 app.get("/", (req,res) => {
     console.log("Response to the root directory has been detected!");
     // Here we use the public directory after we have related it to the index file.
-    const index = path.join(__dirname, "public", "index.html");
+    const index = path.join(__dirname, "../public", "index.html");
     res.status(200).sendFile(index);
 });
 
